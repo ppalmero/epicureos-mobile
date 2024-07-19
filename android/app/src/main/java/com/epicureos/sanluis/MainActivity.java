0300fd151e527bdf44f9a014f4955ffbc29bbedb
ppalmero <pablorafaelpalmero@gmail.com>
2024-07-19 13:23:54 -0300

    first

---
+++ android/app/src/main/java/com/epicureos/sanluis/MainActivity.java
@@ -0,0 +1,5 @@
+package com.epicureos.sanluis;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.epicureos.sanluis',
+  appName: 'epicureos-mobile',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
